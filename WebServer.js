cdf3aac6139e73b1c7866593be631d6023ea7c1e
agu <agu@pqqqqw.pw>
2016-11-08 11:23:13 +0100

    Cambiado el servidor de prueba por el real
    Se ha eliminado el servidor de prueba y añadido el servidor real, con
    enrutamiento para las peticiones a la API y usando ExpressJS.

---
+++ WebServer.js
@@ -0,0 +1,29 @@
+﻿var express = require("express");
+var bodyparser = require("body-parser");
+
+//Creamos una instancia del servidor
+var server = express();
+
+const port = 80;
+
+//Usamos bodyparser en el servidor para poder extraer los json de las peticiones POST
+server.use(bodyparser.urlencoded({ extended: true }));
+server.use(bodyparser.json());
+
+//Enrutador de peticiones para la API
+var router = express.Router();
+
+router.get("/api/recontarEncuesta", (request, response) => {
+	response.json({status: "ok"});
+	//Completar...
+});
+
+router.post("/api/modificarEncuesta", (request, response) => {
+	response.json({status: "ok"});
+	//Completar...
+});
+
+server.use(router);
+server.listen(port, () => {
+	console.log("Servidor iniciado en el puerto " + port);
+});
