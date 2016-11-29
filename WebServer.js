3a0509bc1de8add893b499e432b03a5529f65cf8
David de los Santos Boix <David de los Santos Boix>
2016-11-29 22:33:09 +0100

    Parámetros para OpenShift

--- WebServer.js
+++ WebServer.js
@@ -153,6 +153,9 @@ server.use(router);
 //Para las restantes rutas no especificadas, usar el manejador de 404
 server.use(display404error);
 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 server.listen(port, () => {
 	console.log("Servidor iniciado en el puerto " + port);
 });
