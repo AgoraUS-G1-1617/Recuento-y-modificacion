9575a8e06d851f620ab2ef4cb53e860cbf859794
Rafael García <rafael_94@movistar.es>
2016-11-30 12:40:38 +0100

    Test unitarios utilizando chai
    Adaptación del modulo de modificación para test unitarios.
    Test añadidos a test.js

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var expect    = require("chai").expect;
 var db = require("node-localdb");
 var auth = require("./authModule");
+var modif = require("./modificationModule");
 
 describe("Comprobación del módulo de Autenticación", function() {
 	describe("Test de Consulta de ID", function() {
@@ -24,4 +25,49 @@ describe("Comprobación del módulo de Autenticación", function() {
     		});
     	});
   	});
+});
+
+describe("Tests unitarios módulo modificación", function(){
+	it("comprobación de permisos", function(){
+		modif.checkPermissionsTestPositive().then(function (result){
+			expect(result).to.be.true;
+		});
+		
+	});
+	
+	it("comprobación de permisos", function(){
+		modif.checkPermissionsTestNegative().then(function(result){
+			expect(result).to.be.false;
+		});
+		
+	});
+	
+	it("comprobación de encuesta", function(){
+		modif.checkSurveyTestPositive().then(function(result){
+			expect(result).to.be.true;
+		});
+		
+	});
+	
+	it("comprobación de encuesta", function(){
+		modif.checkSurveyTestNegative().then(function(result){
+			expect(result).to.be.false;
+		});
+		
+	});
+	
+	it("comprobación de modificación de voto", function(){
+		modif.changeVoteTestPositive().then(function(result){
+			expect(result).not.to.be.undefined;
+		});
+	});
+	
+	//console.log("\n")
+	//checkPermissionsTestNegative();
+	//console.log("\n")
+	//checkSurveyTestPositive();
+	//setTimeout(function(){
+	//checkSurveyTestNegative()}, 500);
+	//setTimeout(function(){
+	//changeVoteTestPositive()}, 1000);
 });
