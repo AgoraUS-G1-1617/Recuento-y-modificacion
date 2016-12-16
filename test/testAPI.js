deddc6317778b47f6fc0c1b9fa8a7c1f262fcf23
agubelu <agustinbd3@gmail.com>
2016-12-16 16:00:51 +0100

    Añadidos tests de modificación de votos

--- test/testAPI.js
+++ test/testAPI.js
@@ -9,7 +9,7 @@ describe("Tests de la API", function() {
 	before(function() {
 		regenerateBD();
 	});
-	
+
 	describe("Obtener votacion(es)", function() {
 		
 		it("Consultar todas las votaciones", function() {
@@ -174,7 +174,133 @@ describe("Tests de la API", function() {
         });
     
     });
-	
+
+    describe("Modificar votos", function() {
+        
+        it("Modificación correcta de voto", function() {
+            expect(() => {
+                regenerateBD();
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequestCrearVoto = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&voto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var resCrearVoto = req2obj("/api/emitirVoto", "POST", {body: bodyRequestCrearVoto, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(resCrearVoto.estado).to.equal(201);
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(200);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto en encuesta no participada", function() {
+            expect(() => {
+                regenerateBD();
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[1].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto con nuevo voto sin encriptar", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequestCrearVoto = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&voto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var resCrearVoto = req2obj("/api/emitirVoto", "POST", {body: bodyRequestCrearVoto, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(resCrearVoto.estado).to.equal(201);
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&nuevoVoto=" + encodeURIComponent(pregunta.opciones[0].id_opcion);
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto sin nuevo voto", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta);
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto sin pregunta", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto con pregunta no válida", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=100000&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto sin token", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "test_travisTesting";
+                
+                var bodyRequest = "idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(400);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+        it("Modificación de voto ton token no válido", function() {
+            expect(() => {
+                var votaciones = req2obj("/api/verVotaciones?detallado=si", "GET");
+				expect(votaciones.estado).to.equal(200);
+                var pregunta = votaciones.votaciones[0].preguntas[0];
+                var userToken = "tokenNoValido";
+                
+                var bodyRequest = "token=" + encodeURIComponent(userToken) + "&idPregunta=" + encodeURIComponent(pregunta.id_pregunta) + "&nuevoVoto=" + encodeURIComponent(crypto.encrypt(pregunta.opciones[0].id_opcion));
+                var res = req2obj("/api/modificarVoto", "POST", {body: bodyRequest, headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+                expect(res.estado).to.equal(403);
+                expect(res.mensaje.length > 0).to.be.true;
+            }).to.not.throw("");  
+        });
+        
+    });
+    
 });
 
 function req2obj(url, method, options) {
@@ -182,7 +308,7 @@ function req2obj(url, method, options) {
 	try {
 		return JSON.parse(response.getBody("utf-8"));
 	} catch(err) {
-		//Ignora los códigos de error >400 del servidor y devuelve el cuerpo de la respuesta de todos modos
+		//Ignora los códigos de error >=400 del servidor y devuelve el cuerpo de la respuesta de todos modos
 		return JSON.parse(err.body.toString("utf-8"));
 	}
 }
